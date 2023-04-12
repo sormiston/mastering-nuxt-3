467fe53af2f21b242a18885d9a4a38638375011a
sormiston <sean.david.ormiston@gmail.com>
2023-04-12 18:46:39 +0100

    adds TypeScript to useCourse composable

--- composables/useCourse.ts
+++ composables/useCourse.ts
@@ -1,14 +1,44 @@
 import courseData from './courseData';
 
+type Lesson = {
+  title: string;
+  slug: string;
+  number: number;
+  downloadUrl: string;
+  sourceUrl?: string;
+  videoId: number;
+  text: string;
+  path: string;
+};
+
+type Chapter = {
+  title: string;
+  slug: string;
+  number: number;
+  lessons: Lesson[];
+};
+
+type Course = {
+  title: string;
+  chapters: Chapter[];
+};
+
+
 export const useCourse = () => {
+  const chapters: Chapter[] = courseData.chapters.map((chapter) => {
+    const lessons: Lesson[] = chapter.lessons.map((lesson) => ({
+      ...lesson,
+      path: `/course/chapter/${chapter.slug}/lesson/${lesson.slug}`,
+    } as Lesson))
+
+    return {
+      ...chapter,
+      lessons,
+    } as Chapter
+  });
+
   return {
     ...courseData,
-    chapters: courseData.chapters.map((chapter) => ({
-      ...chapter,
-      lessons: chapter.lessons.map((lesson) => ({
-        ...lesson,
-        path: `/course/chapter/${chapter.slug}/lesson/${lesson.slug}`,
-      })),
-    })),
-  };
+    chapters 
+  } as Course;
 };
