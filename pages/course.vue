89e29602462ed72a061e0967ed834387e18bd676
sormiston <sean.david.ormiston@gmail.com>
2023-04-18 00:32:11 +0100

    adds UserCard component

--- pages/course.vue
+++ pages/course.vue
@@ -1,12 +1,10 @@
 <template>
   <div>
-    <div class="prose mb-12">
-      <h1>
-        <span class="font-medium">
-          Course:
-          <span class="font-bold">{{ title }}</span>
-        </span>
+    <div class="mb-4 flex justify-between items-center w-full">
+      <h1 class="text-3xl">
+        <span class="font-bold">{{ title }}</span>
       </h1>
+      <UserCard />
     </div>
     <div class="flex flex-row justify-center flex-grow">
       <div
