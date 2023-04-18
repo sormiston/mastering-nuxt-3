099a18d1d7fd4df2d599000c142d1539ac426d6f
sormiston <sean.david.ormiston@gmail.com>
2023-04-18 10:24:50 +0100

    adds redirect query param to login page

--- middleware/auth.ts
+++ middleware/auth.ts
@@ -1,5 +1,9 @@
-export default defineNuxtRouteMiddleware((to, from) => {
-    if (to.params.chapterSlug !== "1-chapter-1") {
-        return navigateTo("/login");
-    }
-})
+export default defineNuxtRouteMiddleware(async (to) => {
+  const user = useSupabaseUser();
+
+  if (user.value || to.params.chapterSlug === "1-chapter-1") {
+    return;
+  } else {
+    return navigateTo(`/login?redirectTo=${to.path}`);
+  }
+});
