4bad5b037b675ac1648c933e478ea65b2fda000a
sormiston <sean.david.ormiston@gmail.com>
2023-04-26 19:14:04 +0100

    ⭐ feat(server-middleware): adds server-side HTTP logging with winston

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,11 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-  modules: ['@nuxtjs/tailwindcss', '@vueuse/nuxt', '@nuxtjs/supabase'],
+  modules: ["@nuxtjs/tailwindcss", "@vueuse/nuxt", "@nuxtjs/supabase"],
+  sourcemap: {
+    server: true,
+    client: true,
+  },
+  runtimeConfig: {
+    httpLogs: false,
+  },
 });
---
+++ server/middleware/httpLogger.ts
@@ -0,0 +1,20 @@
+export default defineEventHandler((evt) => {
+  // do nothing if process log-level does not equal http
+  const runtimeConfig = useRuntimeConfig();
+  if (!runtimeConfig.httpLogs) return;
+
+  const logger = getWinstonInstance();
+  const {
+    node: { req, res },
+  } = evt;
+
+  const reducedRequest = {
+    method: req.method,
+    url: req.url,
+    headers: req.headers,
+  };
+  if (logger.http) {
+    logger.http(JSON.stringify(reducedRequest, null, 2));
+  }
+  
+});
