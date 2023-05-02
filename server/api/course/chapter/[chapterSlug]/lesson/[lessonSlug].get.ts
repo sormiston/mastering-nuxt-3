6c08321effb2661e5019ab7492b6aa3520e9d33d
sormiston <sean.david.ormiston@gmail.com>
2023-05-02 22:22:01 +0100

    ⭐ feat(api):  getLesson endpoint + type elevations

---
+++ server/api/course/chapter/[chapterSlug]/lesson/[lessonSlug].get.ts
@@ -0,0 +1,40 @@
+import { getRouterParams } from "h3";
+import { Course, Chapter, Lesson, LessonWithPath } from "@/types/types";
+import course from "@/server/courseData.js";
+
+course as Course;
+
+export default defineEventHandler((event): LessonWithPath => {
+  // Using unjs/h3 utility function to get router params
+  // https://www.jsdocs.io/package/h3#package-index-functions
+  // alternatively, can access event.context.params;
+  const { chapterSlug, lessonSlug } = getRouterParams(event);
+  // alternatively, can access event.context.params;
+  // const { chapterSlug, lessonSlug } = event.context.params;
+  const chapter: Maybe<Chapter> = course.chapters.find(
+    (chapter) => chapter.slug === chapterSlug
+  );
+
+  if (!chapter) {
+    throw createError({
+      statusCode: 404,
+      message: `Chapter not found`,
+    });
+  }
+
+  const lesson: Maybe<Lesson> = chapter.lessons.find(
+    (lesson) => lesson.slug === lessonSlug
+  );
+
+  if (!lesson) {
+    throw createError({
+      statusCode: 404,
+      message: `Lesson not found`,
+    });
+  }
+
+  return {
+    ...lesson,
+    path: `/course/chapter/${chapter.slug}/lesson/${lesson.slug}`,
+  };
+});
