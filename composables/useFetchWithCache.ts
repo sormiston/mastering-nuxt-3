5f835ebdc3640f6c9700d4109bee8c0214cc4db3
sormiston <sean.david.ormiston@gmail.com>
2023-05-03 16:57:38 +0100

    🔨 refactor(useFetchWithCache):  abstracts fetchWithCache as a generic composable

---
+++ composables/useFetchWithCache.ts
@@ -0,0 +1,28 @@
+import { StorageSerializers } from "@vueuse/core";
+
+export default async <T>(url: string) => {
+  const cached = useSessionStorage<T>(url, null, {
+    // use object serializer
+    serializer: StorageSerializers.object,
+  });
+
+  if (!cached.value) {
+    const { data, error } = await useFetch<T>(url);
+
+    if (error.value) {
+      throw createError({
+        ...error.value,
+        statusMessage: `Could not fetch data from ${url}`,
+      });
+    }
+
+    // If we've made it past the error check, we know that data is defined
+    // cache it in sessionStorage
+
+    cached.value = data.value as T;
+  } else {
+    console.log(`Getting value from cache for ${url}`);
+  }
+
+  return cached;
+};
