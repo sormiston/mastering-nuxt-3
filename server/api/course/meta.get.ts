f101080fba36eecb6801b8b9e62e8b8b375a8c04
sormiston <sean.david.ormiston@gmail.com>
2023-05-08 22:49:14 +0100

    hard experiment: if Prisma is removed, can PrismaClient be resolved?

--- server/api/course/meta.get.ts
+++ server/api/course/meta.get.ts
@@ -1,75 +1,70 @@
-import { PrismaClient, Prisma } from '@prisma/client';
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-const lessonSelect = Prisma.validator<Prisma.LessonArgs>()({
+const lessonSelect = {
   select: {
     title: true,
     slug: true,
     number: true,
   },
-});
-export type LessonOutline = Prisma.LessonGetPayload<
-  typeof lessonSelect
-> & {
-  path: string;
 };
+// export type LessonOutline = Prisma.LessonGetPayload<
+//   typeof lessonSelect
+// > & {
+//   path: string;
+// };
 
-const chapterSelect =
-  Prisma.validator<Prisma.ChapterArgs>()({
-    select: {
-      title: true,
-      slug: true,
-      number: true,
-      lessons: lessonSelect,
-    },
-  });
-export type ChapterOutline = Omit<
-  Prisma.ChapterGetPayload<typeof chapterSelect>,
-  'lessons'
-> & {
-  lessons: LessonOutline[];
+const chapterSelect = {
+  select: {
+    title: true,
+    slug: true,
+    number: true,
+    lessons: lessonSelect,
+  },
 };
+// export type ChapterOutline = Omit<
+//   Prisma.ChapterGetPayload<typeof chapterSelect>,
+//   "lessons"
+// > & {
+//   lessons: LessonOutline[];
+// };
 
-const courseSelect = Prisma.validator<Prisma.CourseArgs>()({
+const courseSelect = {
   select: {
     title: true,
     chapters: chapterSelect,
   },
-});
-export type CourseOutline = Omit<
-  Prisma.CourseGetPayload<typeof courseSelect>,
-  'chapters'
-> & {
-  chapters: ChapterOutline[];
-};
+}
+// export type CourseOutline = Omit<
+//   Prisma.CourseGetPayload<typeof courseSelect>,
+//   "chapters"
+// > & {
+//   chapters: ChapterOutline[];
+// };
 
-export default defineEventHandler(
-  async (): Promise<CourseOutline> => {
-    const outline = await prisma.course.findFirst(
-      courseSelect
-    );
+export default defineEventHandler(async () => {
+  const outline = await prisma.course.findFirst(courseSelect);
 
-    // Error if there is no course
-    if (!outline) {
-      throw createError({
-        statusCode: 404,
-        statusMessage: 'Course not found',
-      });
-    }
-
-    // Map the outline so we can add a path to each lesson
-    const chapters = outline.chapters.map((chapter) => ({
-      ...chapter,
-      lessons: chapter.lessons.map((lesson) => ({
-        ...lesson,
-        path: `/course/chapter/${chapter.slug}/lesson/${lesson.slug}`,
-      })),
-    }));
-
-    return {
-      ...outline,
-      chapters,
-    };
+  // Error if there is no course
+  if (!outline) {
+    throw createError({
+      statusCode: 404,
+      statusMessage: "Course not found",
+    });
   }
-);
+
+  // Map the outline so we can add a path to each lesson
+  const chapters = outline.chapters.map((chapter) => ({
+    ...chapter,
+    lessons: chapter.lessons.map((lesson) => ({
+      ...lesson,
+      path: `/course/chapter/${chapter.slug}/lesson/${lesson.slug}`,
+    })),
+  }));
+
+  return {
+    ...outline,
+    chapters,
+  };
+});
